9cb0e0b199a6092f2e51a75f3353d7d9f25a97c1
Didi <didi.zlatkova93@gmail.com>
2015-05-28 17:25:32 +0300

    added slicing and switching

--- src/Solution.java
+++ src/Solution.java
@@ -3,9 +3,14 @@ public class Solution {
 
 	public static void main(String[] args) {
 		// TODO get input
+		String encrypted = "vrItommseIal vihack~416~Ilocveakgrithms he";
 		
-		// TODO slice and switch
+		// slice and switch
+		String slicedAndSwitched = encrypted.substring((int)Math.round(encrypted.length()/2.0), encrypted.length())
+				+ encrypted.substring(0, (int)Math.round(encrypted.length()/2.0));
 		
+		System.out.println(slicedAndSwitched);		
+				
 		// TODO get alphabet, key and encrypted message
 		
 		// TODO make key the length of the message
