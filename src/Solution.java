fe2f08174979662a3ca2b4933be971845d5fc644
Didi <didi.zlatkova93@gmail.com>
2015-05-28 17:14:22 +0300

    added todo comments for steps of the solution

---
+++ src/Solution.java
@@ -0,0 +1,20 @@
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO get input
+		
+		// TODO slice and switch
+		
+		// TODO get alphabet, key and encrypted message
+		
+		// TODO make key the length of the message
+		
+		// TODO remove each key index from each encrypted message index
+		
+		// TODO if negative, add length of the alphabet
+		
+		// TODO get original message 
+	}
+
+}
