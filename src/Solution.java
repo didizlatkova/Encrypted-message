4ada77aea9c75f8951be03eb5ec94eb6ae6f333e
Didi <didi.zlatkova93@gmail.com>
2015-05-29 10:36:15 +0300

    finished the decrypting algorithm

--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,17 @@
+import java.util.Scanner;
+
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO get input
-		String encrypted = "vrItommseIal vihack~416~Ilocveakgrithms he";
+		// get input
+		Scanner sc = new Scanner(System.in);
+		String encrypted = sc.nextLine();
 
 		// slice and switch
 		String slicedAndSwitched = encrypted.substring(
 				(int) Math.round(encrypted.length() / 2.0), encrypted.length())
 				+ encrypted.substring(0,
 						(int) Math.round(encrypted.length() / 2.0));
-
-		System.out.println(slicedAndSwitched);
 
 		// get alphabet length
 		String alphabetCountString = "";
@@ -21,7 +22,6 @@ public class Solution {
 				break;
 			}
 		}
-		System.out.println(alphabetCountString);
 
 		int alphabetCount = Integer.parseInt(alphabetCountString);
 
@@ -29,7 +29,6 @@ public class Solution {
 		String alphabet = slicedAndSwitched.substring(
 				alphabetCountString.length() + 1, alphabetCountString.length()
 						+ 1 + alphabetCount);
-		System.out.println("[" + alphabet + "]");
 
 		// get key
 		String keyCountString = "";
@@ -42,7 +41,6 @@ public class Solution {
 		}
 
 		keyCountString = new StringBuilder(keyCountString).reverse().toString();
-		System.out.println(keyCountString);
 
 		int keyCount = Integer.parseInt(keyCountString);
 
@@ -50,23 +48,42 @@ public class Solution {
 		String key = slicedAndSwitched.substring(slicedAndSwitched.length() - 1
 				- keyCountString.length() - keyCount,
 				slicedAndSwitched.length() - 1 - keyCountString.length());
-		System.out.println("[" + key + "]");
 
 		// get encrypted message
 		String encryptedMessage = slicedAndSwitched.substring(
 				alphabetCountString.length() + 1 + alphabet.length(),
 				slicedAndSwitched.length() - 1 - keyCountString.length()
 						- key.length());
+
+		// make key the length of the message
+		int timesToAdd = (int) Math.ceil((double) encryptedMessage.length()
+				/ (double) key.length()) - 1;
+		String saveKey = key;
+		for (int i = 0; i < timesToAdd; i++) {
+			key += saveKey;
+		}
+
+		key = key.substring(0, encryptedMessage.length());
+
+		// remove each key index from each encrypted message index
+		int[] originalMessageIndexes = new int[encryptedMessage.length()];
+		int index = 0;
+		for (int i = 0; i < encryptedMessage.length(); i++) {
+			index = alphabet.indexOf(encryptedMessage.charAt(i))
+					- alphabet.indexOf(key.charAt(i));
+			if (index < 0) {
+				index = index + alphabet.length();
+			}
+
+			originalMessageIndexes[i] = index;
+		}
+
+		// get original message
+		for (int i = 0; i < originalMessageIndexes.length; i++) {
+			System.out.print(alphabet.charAt(originalMessageIndexes[i]));
+		}
 		
-		System.out.println("[" + encryptedMessage + "]");
-
-		// TODO make key the length of the message
-
-		// TODO remove each key index from each encrypted message index
-
-		// TODO if negative, add length of the alphabet
-
-		// TODO get original message
+		sc.close();
 	}
 
 }
